c857cabd67a2dad97bb4ee2f40584a5a78f49ca9
maybeizen <me@maybeizen.space>
2026-08-01 00:09:20 -0400

    fix: remove test bot (and fix other stuff)

--- apps/test-bot/conflux.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "@confluxjs/conflux/config";
-
-export default defineConfig({
-  commandsDir: "src/commands",
-  eventsDir: "src/events",
-  token: process.env.BOT_TOKEN,
-  outDir: "dist",
-});
--- apps/test-bot/src/commands/say.ts
+++
@@ -1,9 +0,0 @@
-import type { MessageCommand } from "@confluxjs/conflux/commands";
-
-export const data = {
-  name: "say",
-};
-
-export const message: MessageCommand = async (ctx) => {
-  await ctx.message.reply(ctx.args().join(" "));
-};
